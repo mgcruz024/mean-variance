c43bd135834ce39dfb5808ab0fded1ce62d3d0ed
Miguel Cruz <107597357+mgcruz024@users.noreply.github.com>
2023-09-18 02:35:28 -0700

    MV Optimization

--- MV Analysis/mv-optimization.R
+++ MV Analysis/mv-optimization.R
@@ -1,16 +1,15 @@
 
-
-rm(list=ls(all=T)) # this just removes everything from memory
+rm(list=ls(all=T)) 
 
 # Tabular Return Data Analytics
 
-# We will select 'SP500TR' and our group tickers
+# 'SP500TR' and chosen tickers. 
 require(PerformanceAnalytics)
 require(PortfolioAnalytics)
 require(ROI) 
 require(ROI.plugin.quadprog)
 
-# We need to convert data frames to xts (extensible time series)
+# Convert data frames to xts (extensible time series)
 Ra<-as.xts(eod_ret[,c("PEP","PG","PGR","AAPL","AMZN","AMD","ADP",
 "ADBE","ABBV","CRY","CSCO","CPA"),drop=F])
 
@@ -39,7 +38,7 @@ nrow(Rb_training)
 Ra_testing<-tail(Ra,58)
 Rb_testing<-tail(Rb,58)
 
-#optimize the MV (Markowitz 1950s) portfolio weights based on training
+# optimize the MV (Markowitz 1950s) portfolio weights based on training
 
 table.AnnualizedReturns(Ra_training)
 mar_ra<-mean(Ra_training) #daily minimum acceptable return for our stocks during Range 1
@@ -48,30 +47,26 @@ table.AnnualizedReturns(Rb_training)
 mar_rb<-mean(Rb_training) #daily minimum acceptable return for SP500TR during Range 1
 
 
-
-# Q1 Cumulative return chart for Project Range #1 (2016-2020) for 
-# stock tickers selected by your team 
+# Cumulative return chart for 2016-2020
 
 chart.CumReturns(Ra_training,legend.loc = 'topleft')
 
 
-# Q2 Weights of your optimized portfolio (four digits precision) and the sum of these weights
-# based on Project Range #1 
-
+# Weights of optimized portfolio and the sum of these weights based on 2016-2020
 
 pspec<-portfolio.spec(assets=colnames(Ra_training))
 pspec<-add.objective(portfolio=pspec,type="risk",name='StdDev')
 pspec<-add.constraint(portfolio=pspec,type="full_investment")
 pspec<-add.constraint(portfolio=pspec,type="return",return_target=mar_rb)
 
-#optimize portfolio for Jan 2016 to Dec 2020
+# optimize portfolio for Jan 2016 to Dec 2020
 opt_p<-optimize.portfolio(R=Ra_training,portfolio=pspec,optimize_method = 'ROI', digits=4)
 opt_w<-round(opt_p$weights, digits = 4) #optimum weights
 sum(opt_p$weights)
 
 
-# Q3 Cumulative return chart for your optimized portfolio and SP500TR index for all available
-# 2021 data (Project Range #2) 
+# Cumulative return chart for optimized portfolio and SP500TR index for all available
+# 2021 data 
            
 Rp<-Rb_testing # easier to apply the existing structure
 #define new column that is the dot product of the two vectors
@@ -79,7 +74,7 @@ Rp$Portfolio<-Ra_testing %*% opt_w
 
 chart.CumReturns(Rp,legend.loc = 'bottomright')
 
-# Q4 Annualized returns for your portfolio and SP500TR index for Project Range #2
+# Annualized returns for portfolio and SP500TR index for 2021 data 
 
 table.AnnualizedReturns(Rp)
 
