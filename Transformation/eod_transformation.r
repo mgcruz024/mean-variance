bf37f38c0cd4ceeea807a7316d2809f51df3a99d
Miguel Cruz <mgcruz024@gmail.com>
2023-09-17 19:32:02 -0700

    Transforming EOD SP500 data into daily returns on R.

---
+++ Transformation/eod_transformation.r
@@ -0,0 +1,128 @@
+# MV Portfolio Project in R
+rm(list=ls(all=T)) # this just removes everything from memory
+
+
+
+require(RPostgres) # did you install this package?
+require(DBI)
+conn <- dbConnect(RPostgres::Postgres()
+                 ,user="stockmarketreader"
+                 ,password="read123"
+                 ,host="localhost"
+                 ,port=5432
+                 ,dbname="mvportfolio"
+)
+
+#custom calendar
+qry<-'SELECT * FROM custom_calendar ORDER by date'
+ccal<-dbGetQuery(conn,qry)
+#eod prices and indices
+qry1="SELECT symbol,date,adj_close FROM eod_indices WHERE date BETWEEN '2015-12-31' AND '2021-12-31'"
+qry2="SELECT ticker,date,adj_close FROM eod_quotes WHERE date BETWEEN '2015-12-31' AND '2021-12-31'"
+eod<-dbGetQuery(conn,paste(qry1,'UNION',qry2))
+dbDisconnect(conn)
+rm(conn)
+
+
+
+#Adding one more data item (for 2015-12-31)
+eod_row<-data.frame(symbol='SP500TR',date=as.Date('2015-12-31'),adj_close=3821.60)
+eod<-rbind(eod,eod_row)
+tail(eod)
+
+
+# Use Calendar --------------------------------------------------------
+
+tdays<-ccal[which(ccal$trading==1),,drop=F]
+head(tdays)
+nrow(tdays)-1 #trading days between 2016 and 2021
+
+# Completeness ----------------------------------------------------------
+# Percentage of completeness
+pct<-table(eod$symbol)/(nrow(tdays)-1)
+selected_symbols_daily<-names(pct)[which(pct>=0.99)]
+eod_complete<-eod[which(eod$symbol %in% selected_symbols_daily),,drop=F]
+
+#check
+head(eod_complete)
+tail(eod_complete)
+nrow(eod_complete)
+
+# Transform (Pivot) -------------------------------------------------------
+
+require(reshape2) #did you install this package?
+eod_pvt<-dcast(eod_complete, date ~ symbol,value.var='adj_close',fun.aggregate = mean, fill=NULL)
+#check
+eod_pvt[1:10,1:5] #first 10 rows and first 5 columns 
+ncol(eod_pvt) # column count
+nrow(eod_pvt)
+
+# Merge with Calendar -----------------------------------------------------
+eod_pvt_complete<-merge.data.frame(x=tdays[,'date',drop=F],y=eod_pvt,by='date',all.x=T)
+
+#check
+eod_pvt_complete[1:10,1:5] #first 10 rows and first 5 columns 
+ncol(eod_pvt_complete)
+nrow(eod_pvt_complete)
+
+#use dates as row names and remove the date column
+rownames(eod_pvt_complete)<-eod_pvt_complete$date
+eod_pvt_complete$date<-NULL #remove the "date" column
+
+#re-check
+eod_pvt_complete[1:10,1:5] #first 10 rows and first 5 columns 
+ncol(eod_pvt_complete)
+nrow(eod_pvt_complete)
+
+# Missing Data Imputation -----------------------------------------------------
+# We can replace a few missing (NA or NaN) data items with previous data
+# Let's say no more than 3 in a row...
+require(zoo)
+eod_pvt_complete<-na.locf(eod_pvt_complete,na.rm=F,fromLast=F,maxgap=3)
+#re-check
+eod_pvt_complete[1:10,1:5] #first 10 rows and first 5 columns 
+ncol(eod_pvt_complete)
+nrow(eod_pvt_complete)
+
+# Calculating Returns -----------------------------------------------------
+require(PerformanceAnalytics)
+eod_ret<-CalculateReturns(eod_pvt_complete)
+
+#check
+eod_ret[1:10,1:3] #first 10 rows and first 3 columns 
+ncol(eod_ret)
+nrow(eod_ret)
+
+#remove the first row
+eod_ret<-tail(eod_ret,-1) #use tail with a negative value
+#check
+eod_ret[1:10,1:3] #first 10 rows and first 3 columns 
+ncol(eod_ret)
+nrow(eod_ret)
+
+
+# Check for extreme returns -------------------------------------------
+# There is colSums, colMeans but no colMax so we need to create it
+colMax <- function(data) sapply(data, max, na.rm = TRUE)
+# Apply it
+max_daily_ret<-colMax(eod_ret)
+max_daily_ret[1:10] #first 10 max returns
+# And proceed just like we did with percentage (completeness)
+selected_symbols_daily<-names(max_daily_ret)[which(max_daily_ret<=1.00)]
+length(selected_symbols_daily)
+
+#subset eod_ret
+eod_ret<-eod_ret[,which(colnames(eod_ret) %in% selected_symbols_daily),drop=F]
+#check
+eod_ret[1:10,1:3] #first 10 rows and first 3 columns 
+ncol(eod_ret)
+nrow(eod_ret)
+
+#YOUR TURN: subset eom_ret data
+
+# Export data from R to CSV -----------------------------------------------
+write.csv(eod_ret,'C:/temp/eod_ret.csv')
+
+# You can actually open this file in Excel!
+
+
